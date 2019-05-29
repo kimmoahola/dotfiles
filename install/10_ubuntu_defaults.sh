0b089ab0d2b1569b1998854046ab7f3f206202ca
kimmoahola <28525546+kimmoahola@users.noreply.github.com>
2019-05-29 22:10:10 +0300

    Disable ubuntu terminal system font

--- install/10_ubuntu_defaults.sh
+++ install/10_ubuntu_defaults.sh
@@ -10,6 +10,7 @@ set -x
 profile=$(gsettings get org.gnome.Terminal.ProfilesList default)
 profile=${profile:1:-1} # remove leading and trailing single quotes
 dconf write /org/gnome/terminal/legacy/profiles:/:$profile/font "'Noto Mono for Powerline 10'"
+dconf write /org/gnome/terminal/legacy/profiles:/:$profile/use-system-font false
 
 dconf write /org/gnome/terminal/legacy/theme-variant "'dark'"
 
