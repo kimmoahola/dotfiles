ee993ed263019ae7a481293d6c50e7379d6c6020
kimmoahola <28525546+kimmoahola@users.noreply.github.com>
2019-05-31 22:21:41 +0300

    Avoid downloading docker-compose if  not needed

--- install/03_desktop_packages.sh
+++ install/03_desktop_packages.sh
@@ -63,8 +63,12 @@ if is-ubuntu; then
 
   sudo apt-get --assume-yes install "${apps[@]}"
 
-  sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
+  DOCKER_COMPOSE_VERSION="1.24.0"
+  if ! [ -f /usr/local/bin/docker-compose-$DOCKER_COMPOSE_VERSION ]; then
+    sudo curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose-$DOCKER_COMPOSE_VERSION
+  fi
+  sudo chmod +x /usr/local/bin/docker-compose-$DOCKER_COMPOSE_VERSION
+  sudo ln -sfv /usr/local/bin/docker-compose-$DOCKER_COMPOSE_VERSION /usr/local/bin/docker-compose
 
   snap_apps=(
     insomnia
