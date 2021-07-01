a3b703349a8cb38f26de538bbca69549eb6b6029
kimmoahola <28525546+kimmoahola@users.noreply.github.com>
2021-07-01 12:09:14 +0300

    Add functions cheat and clean-docker

--- system/function.fish
+++ system/function.fish
@@ -10,3 +10,17 @@ end
 function calc
   echo $argv | bc -l
 end
+
+# Usage:
+# cheat tar~list
+function cheat
+  curl cheat.sh/"$argv"
+end
+
+function clean-docker
+  docker ps -a | grep 'hours ago' | awk '{print $1}' | xargs docker rm
+  docker ps -a | grep '.. minutes ago' | awk '{print $1}' | xargs docker rm
+  docker ps -q -a -f status=exited | xargs -n 100 docker rm -v
+  docker images -q --filter "dangling=true" | xargs -n 100 docker rmi
+  docker images | grep "^<none>" | awk '{print $3}' | xargs docker rmi
+end
