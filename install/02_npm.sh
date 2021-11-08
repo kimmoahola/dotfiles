b8392214f55d4d5408652737bcd2a86b4c4d6b5b
kimmoahola <28525546+kimmoahola@users.noreply.github.com>
2021-11-08 14:07:35 +0200

    Fix fnm

--- install/02_npm.sh
+++ install/02_npm.sh
@@ -4,6 +4,7 @@
 curl -fsSL https://fnm.vercel.app/install | bash -s -- --skip-shell
 
 # Run this so we can run fnm right now without opening a new shell
+export PATH="~/.fnm:$PATH"
 eval "$(fnm env)"
 
 fnm install 16
