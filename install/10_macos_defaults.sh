5db3dc618149131fcde8da43756d08252499bfbd
kimmoahola <28525546+kimmoahola@users.noreply.github.com>
2022-11-07 15:14:09 +0200

    Change wvous-br-corner

--- install/10_macos_defaults.sh
+++ install/10_macos_defaults.sh
@@ -192,7 +192,7 @@ defaults write com.apple.dock wvous-bl-corner -int 5
 defaults write com.apple.dock wvous-bl-modifier -int 0
 
 # Bottom right screen corner
-defaults write com.apple.dock wvous-br-corner -int 0
+defaults write com.apple.dock wvous-br-corner -int 10
 defaults write com.apple.dock wvous-br-modifier -int 0
 
 ###############################################################################
